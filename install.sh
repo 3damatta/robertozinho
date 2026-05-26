4437eecf899d761ef8bd09c8076befcc6b2b35bc
3damatta <gmatta40@gmail.com>
2026-05-26 16:34:40 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ set -o pipefail # Garante que erros em pipes sejam detectados
 
 # --- Configurações Iniciais ---
 REPO_OWNER="3damatta"
-REPO_NAME="robertinho"
+REPO_NAME="robertozinho"
 INSTALL_DIR="/opt/icerobot"
 NODE_VERSION="20" # Versão do Node.js a ser instalada
 
